956e7e801955027a965a8250555a40ad9a362e47
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2017-03-28 07:58:35 +0200

    Add zip functions

--- Auxiliaries.elm
+++ Auxiliaries.elm
@@ -4,7 +4,6 @@ import Basics exposing ( identity, toFloat )
 import List exposing   ( foldr, sum, map )
 
 {- Flatten the Maybes in a list, so that Nothings are removed and Justs are unwrapped. -}
-
 catMaybes : List (Maybe a) -> List a
 catMaybes = 
   let f m = case m of
@@ -26,3 +25,16 @@ truncateAt pos x =
 {- Sum all values obtained by applying the given function to every element in the list. -}
 sumMap : (a -> number) -> List a -> number
 sumMap f = sum << map f
+
+{- Zip lists by applying a given function to the respective positions in the list.
+   The length of the result list is the length of the shorter list. -}
+zipWith : (a -> b -> c) -> List a -> List b -> List c
+zipWith f = let go l m = case (l, m) of
+                             (x :: xs, y :: ys) -> f x y :: go xs ys
+                             (_,       _)       -> []
+            in go
+
+{- Combine two lists into a list of pairs.
+   The length of the result is the length of the shorter list. -}
+zip : List a -> List b -> List (a, b)
+zip = zipWith (,)
